6d89ba42ecef3baf88d4946220fa2546a019165a
yasas20050110 <yasassanjaya9@gmaili.com>
2025-12-22 17:17:08 +0530

    Initialize Project

---
+++ src/main/java/com/soul/Point_Of_Sale/PointOfSaleApplication.java
@@ -0,0 +1,13 @@
+package com.soul.Point_Of_Sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soul/Point_Of_Sale/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soul.Point_Of_Sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
